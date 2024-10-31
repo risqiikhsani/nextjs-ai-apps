c8fcc419c1177d5290e0c1b36d529e8beef7e1f6
risqiikhsani <risqiikhsani16@gmail.com>
2024-10-31 15:37:55 +0700

    tst

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // (Optional) Export as a static site
+  // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  //output: 'export', // Feel free to modify/remove this option
+
+  // Override the default webpack configuration
+  // webpack: (config) => {
+  //     // See https://webpack.js.org/configuration/resolve/#resolvealias
+  //     config.resolve.alias = {
+  //         ...config.resolve.alias,
+  //         "sharp$": false,
+  //         "onnxruntime-node$": false,
+  //     }
+  //     return config;
+  // },
 };
 
 export default nextConfig;
